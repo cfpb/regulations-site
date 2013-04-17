3a2ea4e7a7d457e45a793e85d77a666e8374d58b
CM Lubinski <cm.lubinski@gmail.com>
2013-04-17 21:50:46 +0000

    Remove external references

--- static_html_generation/api_stub.py
+++ static_html_generation/api_stub.py
@@ -1,1 +1,6 @@
-/vagrant/code/regulations-parser/api_stub.py
+import codecs
+import json
+
+def get_regulation_as_json(regulations_json_file):
+    f = codecs.open(regulations_json_file, encoding='utf-8')
+    return json.load(f)
