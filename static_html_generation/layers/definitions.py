81372277dc90e682912a070918dfbe46a8881ddb
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-04-17 19:19:28 +0000

    Added definition markup

--- static_html_generation/layers/definitions.py
+++ static_html_generation/layers/definitions.py
@@ -1,4 +1,24 @@
+from internal_citation import InternalCitationLayer
 
 class DefinitionsLayer(object):
-   
-   def apply_layers(self, text, text_index):
+    def __init__(self, layer):
+        self.layer = layer
+
+    def get_definition_citation(self, definition_reference):
+        return definition_reference.split(':')[1].split('-')
+        
+    def apply_layer(self, text, text_index):
+        if text_index in self.layer:
+            layer_elements = self.layer[text_index]
+
+            layer_pairs = []
+            for layer_element in layer_elements:
+                for start, end in layer_element['offsets']:
+                    ot = text[int(start):int(end)]
+                    definition_ref = layer_element['ref']
+                    definition_citation = self.get_definition_citation(definition_ref)
+
+                    le  = {'citation':definition_citation}
+                    rt = InternalCitationLayer.create_link(ot, le, template_name='definition_citation.html')
+                    layer_pairs.append((ot, rt))
+            return layer_pairs
