f0b22ee7583f14d37bd9e262190ded4483a2cbca
Theresa Summa <theresa.summa@cfpb.gov>
2013-04-30 22:50:19 +0000

    ending tab war, adding use strict

--- front_end/js/definition-view.js
+++ front_end/js/definition-view.js
@@ -1,27 +1,28 @@
 define("definition-view", ["jquery", "underscore", "backbone", "regs-data"], function($, _, Backbone, RegsData) {
-  var DefinitionView = Backbone.View.extend({
-    className: "open-definition",
-    events: {},
+    "use strict";
+    var DefinitionView = Backbone.View.extend({
+        className: "open-definition",
+        events: {},
 
-    initialize: function() {
-      this.$termLink = $(this.options.termLink);
+        initialize: function() {
+            this.$termLink = $(this.options.termLink);
 
-      this.model = {
-        id: this.options.termId,
-        content: RegsData.retrieve(this.options.termId) 
-      };
+            this.model = {
+                id: this.options.termId,
+                content: RegsData.retrieve(this.options.termId) 
+            };
 
-      this.render();
-    },
+            this.render();
+        },
 
-    render: function() {
-      this.$el.html(this.model.content);
-      $('body').append(this.$el);
+        render: function() {
+            this.$el.html(this.model.content);
+            $('body').append(this.$el);
 
-      return this;
-    }
+            return this;
+        }
 
-  });
+    });
 
-  return DefinitionView;
+    return DefinitionView;
 });
