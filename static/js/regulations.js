d803c3cfe5f09d40898694bc8594497cfa875391
Theresa Summa <theresa.summa@cfpb.gov>
2013-04-30 17:22:39 +0000

    pulling out stub content, changing indentation

--- static/js/regulations.js
+++ static/js/regulations.js
@@ -1,56 +1,54 @@
-require(["jquery", "underscore", "backbone", "regs-data", "sample-json", "definition-view"], function($, _, Backbone, RegsData, JSONObj, DefinitionView) {
+require(["jquery", "underscore", "backbone", "regs-data", "definition-view"], function($, _, Backbone, RegsData,  DefinitionView) {
 
-  // global state objects
-  window.RegsViews = {
-    openDefinitions: {}
-  }; 
+    // global state objects
+    window.RegsViews = {
+        openDefinitions: {}
+    }; 
 
-  $(document).ready(function() {
-    // template stub
-    var template = function(b, p) {
-      $('#' + p).append(b);
-    };
+    $(document).ready(function() {
+        // template stub
+        var template = function(b, p) {
+            $('#' + p).append(b);
+        };
 
-    if (typeof JSONObj !== 'undefined') {
-      // test data
-      if (navigator.userAgent.indexOf('PhantomJS') < 0) {
-        RegsData.parse(JSONObj); 
-      }
-
-      /* 
-       * EVENT BINDINGS 
-       */
-      // click term link, open definition
-      $('.definition').on('click', function(e) {
-        e.preventDefault();
-        var defId = $(this).attr('data-definition');
-
-        // briefly considered giving the term link its own view
-        // decided that it was unecessary for now. if this event
-        // binding section gets out of hand, we should reconsider [ts]
-
-        // TODO: supports only one open definition
-        if (!RegsViews.openDefinitions[defId]) {
-          RegsViews.openDefinitions[defId] = new DefinitionView({
-            termId: defId,
-            termLink: e.target
-          });
+        // test data
+        if (navigator.userAgent.indexOf('PhantomJS') < 0) {
+            RegsData.parse(JSONObj); 
         }
-        else {
-          RegsViews.openDefinitions[defId].remove();
-          delete(RegsViews.openDefinitions[defId]);
-        }
-      });
 
-      // test "V" links
-      $('.expand').on('click', function(e) {
-        e.preventDefault();
-        var pid = $(this).parent().attr('id'),
-            body = RegsData.retrieve(pid); 
-        template(body, pid);
+        /* 
+        * EVENT BINDINGS 
+        */
+        // click term link, open definition
+        $('.definition').on('click', function(e) {
+            e.preventDefault();
+            var defId = $(this).attr('data-definition');
 
-        $(this).remove();
-      });
-    }
-  });
+            // briefly considered giving the term link its own view
+            // decided that it was unecessary for now. if this event
+            // binding section gets out of hand, we should reconsider [ts]
+
+            // TODO: supports only one open definition
+            if (!RegsViews.openDefinitions[defId]) {
+                RegsViews.openDefinitions[defId] = new DefinitionView({
+                    termId: defId,
+                    termLink: e.target
+                });
+            }
+            else {
+                RegsViews.openDefinitions[defId].remove();
+                delete(RegsViews.openDefinitions[defId]);
+            }
+        });
+
+        // mimics 'read more' accordion type thing
+        $('.expand').on('click', function(e) {
+            e.preventDefault();
+            var pid = $(this).parent().attr('id'),
+                body = RegsData.retrieve(pid); 
+            template(body, pid);
+
+            $(this).remove();
+        });
+    });
 });
