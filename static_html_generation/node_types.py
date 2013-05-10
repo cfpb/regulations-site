47a96611137d1f81d42713678ebafe918b9ae172
Theresa Summa <theresa.summa@cfpb.gov>
2013-05-10 14:39:06 +0000

    comments, changes per #25

--- static_html_generation/node_types.py
+++ static_html_generation/node_types.py
@@ -1,10 +1,21 @@
 class NodeTypes():
     def __init__(self):
+        """ This being a dict assumes that there are other
+        node types that will need id alteration. if not,
+        this can become less generic. """
+
         self.node_types_map = {'Interpretations': 'I'}
          
     def change_type_names(self, id_parts):
+        """ Written originally to change the id representation
+        of Interpretations on the front end vs. what is in the
+        tree. The pattern may prove useful when we've fleshed
+        out more node types. "1005-Interpretations" -> "I-1005"
+        [ts] """
+
+        node_elements = id_parts
         for k, v in self.node_types_map.items():
-            if id_parts.count(k): 
-                id_parts.remove(k)
-                id_parts.insert(0, v)
-        return "-".join(id_parts)
+            if node_elements.count(k): 
+                node_elements.remove(k)
+                node_elements.insert(0, v)
+        return "-".join(node_elements)
