5bed5b7f86a5cc1bd8f1fee11fca9878309b1d73
Theresa Summa <theresa.summa@cfpb.gov>
2013-05-10 20:00:24 +0000

    adding test for node_types

---
+++ static_html_generation/tests/node_types_tests.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from node_types import NodeTypes
+from unittest import TestCase
+
+class NodeTypesTest(TestCase):
+    def test_change_type_names(self):
+        self.node_types = NodeTypes()
+
+        node_parts_before = ['1005', 'Interpretations', '3', '(b)']
+        node_parts_after = self.node_types.change_type_names(node_parts_before)
+        node_string = "-".join(node_parts_after)
+
+        self.assertEqual('I-1005-3-(b)', node_string) 
+        self.assertEqual("I", node_parts_after[0])
