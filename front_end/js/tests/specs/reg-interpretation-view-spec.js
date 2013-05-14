75d91f46fc0fe435994cdeb0f7ab9995bc721f33
Theresa Summa <theresa.summa@cfpb.gov>
2013-05-14 18:30:24 +0000

    adding test for interpretation view

---
+++ front_end/js/tests/specs/reg-interpretation-view-spec.js
@@ -0,0 +1,16 @@
+define(["underscore", "backbone", "jquery", "regulations", "regs-state", "interpretation-view", "regs-data", 'samplejson'], function(_, Backbone, $, RegsApp, RegsState, InterpretationView, RegsData, testjson) {
+  describe("Interpretation views", function() {
+    RegsData.parse(testjson);
+
+    $('body')
+        .append('<div id="I-2345-10">sdfsd</div>')
+        .append('<li id="2345-10"><a href="I-2345-10" class="interpretation-ref">sdfds</a></li>');
+
+    it("should have an initialized view", function() {
+        $('.interpretation-ref').click();   
+
+        expect(RegsState.openInterps['I-2345-10']).toBeTruthy();
+    });
+    
+  });
+});
