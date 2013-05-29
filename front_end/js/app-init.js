4e3f44ba67e44dd4c66eba7ebb4de148f4f10bf0
Theresa Summa <theresa.summa@cfpb.gov>
2013-05-29 23:03:08 +0000

    removing a console call

--- front_end/js/app-init.js
+++ front_end/js/app-init.js
@@ -119,7 +119,6 @@ define(["jquery", "underscore", "backbone", "regs-state", "regs-data", "definiti
         init: function() {
             this.getTree($('#reg-content')); 
             this.bindEvents();
-            console.log(RegsData);
         }
     }
 });
