210d4cfe08d35c015c28bed1c3f25f5e3fff05cf
Theresa Summa <theresa.summa@cfpb.gov>
2013-05-29 23:33:47 +0000

    killing trailing comma

--- front_end/js/interpretation-view.js
+++ front_end/js/interpretation-view.js
@@ -1,7 +1,7 @@
 define("interpretation-view", ['jquery', 'underscore', 'backbone', 'regs-view', 'regs-data'], function($, _, Backbone, RegsView, RegsData) {
     "use strict";
     var InterpretationView = RegsView.extend({
-        className: "open-interpretation",
+        className: "open-interpretation"
     });
 
     return InterpretationView;
