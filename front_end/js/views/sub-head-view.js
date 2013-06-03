bcbd2ff568d20e9e916feadb725eadc567243ae5
Theresa Summa <theresa.summa@cfpb.gov>
2013-06-03 22:09:34 +0000

    rearranging

--- front_end/js/views/sub-head-view.js
+++ front_end/js/views/sub-head-view.js
@@ -2,6 +2,11 @@ define("sub-head-view", ['jquery', 'underscore', 'backbone', 'regs-fixed-el-view
     "use strict";
     var SubHeadView = RegsFixedElView.extend({
         initialize: function() {
+            // phantomjs workaround
+            if (this.$el.length === 0) {
+                this.$el = $('body').append('#sub-head');
+            }
+
             this.menuOffset = this.$el.offset().top;
             Events.on('contract', this.contract, this);
             Events.on('expand', this.expand, this);
