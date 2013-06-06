3292bdd829ae963005f47346a4d0c91374a8e8c6
Theresa Summa <theresa.summa@cfpb.gov>
2013-06-06 21:17:32 +0000

    moving all html gen cmd params to be optional

--- static_html_generation/settings.py
+++ static_html_generation/settings.py
@@ -3,6 +3,10 @@ GOOGLE_ANALYTICS_SITE = ''
 API_BASE = ''
 
 OUTPUT_DIR = '/tmp/'
+TITLE_PART_NUMBER = '1005'
+REG_VERSION = 'remittances'
+EFT_ACT = ['15', '1693'] #Title 15, Section 1693 of the United States Code
+
 
 try:
     from local_settings import * 
