42a13dae9d21e6cf88fc0402bcb21d08c3464ad8
Theresa Summa <theresa.summa@cfpb.gov>
2013-06-27 21:11:24 +0000

    toc tracking

--- front_end/js/source/views/analytics-handler-view.js
+++ front_end/js/source/views/analytics-handler-view.js
@@ -6,15 +6,27 @@ define('analytics-handler', ['jquery', 'underscore', 'backbone', 'regs-dispatch'
             this.bindListeners();
         },
 
-        sendTOCEvent: function() {
-            ga('send', 'event', 'toc', 'clicked');
+        sendEvent: function(e) {
+            var object, action;
+            action = e.data.action;
+            object = e.data.object;
+
+            if (typeof e.data.action === 'function') {
+                action = e.data.action.call();
+            }
+
+            if (typeof e.data.object === 'function') {
+                
+            }
+
+            ga('send', 'event', object, action);
         },
 
         bindListeners: function() {
-            var toc;
-
-            toc = document.getElementById('menu-link');
-            toc.addEventListener('click', this.sendTOCEvent, false);
+            $('#menu-link').on('click', {object: 'toc', action: function() {
+                return $('#menu').hasClass('active') ? 'close' : 'open';
+            }}, this.sendEvent);
+            $('#toc-close').on('click', {object: 'toc', action: 'close-bottom'}, this.sendEvent);
         }
     });
 
