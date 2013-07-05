eff0e7cd7b8243870beb25761e45c6a5e9b11f15
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-07-05 19:24:58 +0000

    Added a section-based url

--- regserver/regserver/urls.py
+++ regserver/regserver/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 
+from regulations.views import RegulationView
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^regulation/(?P<reg_part_section>[-\d\w]+)(?P<reg_version>[-\d\w]+)$', 
+        RegulationView.as_view(), 
+        name='regulation_view'),
     # Examples:
     # url(r'^$', 'regserver.views.home', name='home'),
     # url(r'^regserver/', include('regserver.foo.urls')),
