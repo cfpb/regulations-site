c32d28b66866b31a5445dc983535ada715f54f93
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-07-19 17:52:15 +0000

    Added a way to generate the whole regulation, refactored code so that we reuse

--- regserver/regulations/tests/views_tests.py
+++ regserver/regulations/tests/views_tests.py
@@ -20,3 +20,31 @@ class RegulationParagraphViewTests(TestCase):
             reg_version = 'verver')
         self.assertEqual(context['node'],
                 generator.get_tree_paragraph.return_value)
+
+
+class ViewTests(TestCase):
+    def test_build_context(self):
+        builder = HTMLBuilder(InlineLayersApplier(), 
+                ParagraphLayersApplier(), 
+                SearchReplaceLayersApplier())
+
+        context = {'reg_part':''}
+        new_context = build_context(context, builder)
+        self.assertEquals(new_context.keys(), ['GOOGLE_ANALYTICS_ID', 'tree', 'env', 
+                                                'reg_part', 'GOOGLE_ANALYTICS_SITE'])
+
+    def test_generate_html(self):
+        regulation_tree = {'text': '', 'children': [], 'label': {
+            'text': '8675', 
+            'parts': ['8675'],
+            'title': 'Regulation R'
+        }}
+        i_applier = InlineLayersApplier()
+        p_applier = ParagraphLayersApplier()
+        sr_applier = SearchReplaceLayersApplier()
+        appliers = (i_applier, p_applier, sr_applier)
+        builder = generate_html(regulation_tree, appliers)
+        self.assertEquals(builder.tree, regulation_tree)
+        self.assertEquals(builder.inline_applier, i_applier)
+        self.assertEquals(builder.p_applier, p_applier)
+        self.assertEquals(builder.search_applier, sr_applier)
--- regserver/regulations/views.py
+++ regserver/regulations/views.py
@@ -4,8 +4,38 @@ from django.views.generic.base import TemplateView
 from regulations.generator import generator
 from regulations.generator.html_builder import HTMLBuilder
 
+def generate_html(regulation_tree, layer_appliers):
+    builder = HTMLBuilder(*layer_appliers)
+    builder.tree = regulation_tree
+    builder.generate_html()
+    return builder
+
+def build_context(context, builder):
+    """ Populate a context given an HTMLBuilder object. """
+    context['tree'] = builder.tree
+    context['env'] = builder.get_env_dir()
+    context['GOOGLE_ANALYTICS_SITE'] = settings.GOOGLE_ANALYTICS_SITE
+    context['GOOGLE_ANALYTICS_ID'] = settings.GOOGLE_ANALYTICS_ID
+    return context
+
+class RegulationView(TemplateView):
+    template_name = 'simpler.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RegulationView, self).get_context_data(**kwargs)
+
+        regulation_part = context['reg_part']
+        regulation_version = context['reg_version']
+
+        appliers = generator.get_all_layers(regulation_part, regulation_version)
+        tree = generator.get_regulation(regulation_part, regulation_version)
+
+        builder = generate_html(tree, appliers)
+        context = build_context(context, builder)
+        return context
+
 class RegulationSectionView(TemplateView):
-    template_name = "simpler.html"
+    template_name = 'simpler.html'
 
     @staticmethod
     def get_regulation_part(reg_part_section):
@@ -30,15 +60,8 @@ class RegulationSectionView(TemplateView):
         section_tree = generator.get_regulation_section(regulation, 
                             regulation_version, context['reg_part_section'])
 
-        builder = HTMLBuilder(inline_applier, p_applier, s_applier)
-        builder.tree = section_tree
-        builder.generate_html()
-
-        context['tree'] = builder.tree
-        context['env'] = builder.get_env_dir()
-        context['GOOGLE_ANALYTICS_SITE'] = settings.GOOGLE_ANALYTICS_SITE
-        context['GOOGLE_ANALYTICS_ID'] = settings.GOOGLE_ANALYTICS_ID
-
+        builder = generate_html(section_tree, (inline_applier, p_applier, s_applier))
+        context = build_context(context, builder)
         return context
 
 class RegulationParagraphView(TemplateView):
@@ -54,10 +77,7 @@ class RegulationParagraphView(TemplateView):
         appliers = generator.get_all_section_layers(paragraph_id, version)
         paragraph_tree = generator.get_tree_paragraph(paragraph_id, version)
 
-        builder = HTMLBuilder(*appliers)
-        builder.tree = paragraph_tree
-        builder.generate_html()
+        builder = generate_html(paragraph_tree, appliers)
 
         context['node'] = builder.tree
-
         return context
