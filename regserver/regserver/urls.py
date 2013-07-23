27072ee93a8844095896f222b69b9002a5b0a04a
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-07-23 17:07:47 +0000

    Reorganized the URLs file so that it reuses URL patterns

--- regserver/regserver/urls.py
+++ regserver/regserver/urls.py
@@ -3,29 +3,25 @@ from django.conf.urls import patterns, include, url
 from regulations.views.chrome import RegulationSectionView, RegulationView
 from regulations.views.partial import PartialSectionView, PartialParagraphView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+#Re-usable URL patterns. 
+reg_version_pattern = r'(?P<reg_version>[-\d\w]+)'
+part_section_pattern = r'(?P<reg_part_section>[\d]+[-][\w]+)'
 
 urlpatterns = patterns('',
-    url(r'^regulation/(?P<reg_part_section>[\d]+[-][\w]+)/(?P<reg_version>[-\d\w]+)$', 
+    #A regulation section with chrome
+    url(r'^regulation/%s/%s$' % (part_section_pattern, reg_version_pattern), 
         RegulationSectionView.as_view(), 
         name='regulation_section_view'),
-    url(r'^regulation/(?P<reg_part>[\d]+)/(?P<reg_version>[-\d\w]+)$', 
+    #The whole regulation with chrome
+    url(r'^regulation/(?P<reg_part>[\d]+)/%s$' % reg_version_pattern, 
         RegulationView.as_view(), 
         name='regulation_view'),
-    #url(r'^regulation/(?P<paragraph_id>[-\d\w]+)/(?P<reg_version>[-\d\w]+)$',
-    #    RegulationParagraphView.as_view(),
-    #    name='regulation_paragraph_view'),
-    url(r'^partial/(?P<reg_part_section>[\d]+[-][\w]+)/(?P<reg_version>[-\d\w]+)$', 
+    #A regulation section without chrome
+    url(r'^partial/%s/%s$' % (part_section_pattern, reg_version_pattern), 
         PartialSectionView.as_view(), 
         name='partial_section_view'),
-    url(r'^partial/(?P<paragraph_id>[-\d\w]+)/(?P<reg_version>[-\d\w]+)$',
+    #A regulation paragraph without chrome. 
+    url(r'^partial/(?P<paragraph_id>[-\d\w]+)/%s$' % reg_version_pattern,
         PartialParagraphView.as_view(),
         name='partial_regulation_view'),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
