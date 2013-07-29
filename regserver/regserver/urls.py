0e25168d4233878558adeeb4e5883984d9f07ad3
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-07-29 20:02:05 +0000

    Added examples for better documentation

--- regserver/regserver/urls.py
+++ regserver/regserver/urls.py
@@ -9,18 +9,22 @@ part_section_pattern = r'(?P<reg_part_section>[\d]+[-][\w]+)'
 
 urlpatterns = patterns('',
     #A regulation section with chrome
+    #Example: http://.../regulation/201/2013-10704
     url(r'^regulation/%s/%s$' % (part_section_pattern, reg_version_pattern), 
         RegulationSectionView.as_view(), 
         name='regulation_section_view'),
     #The whole regulation with chrome
+    #Example: http://.../regulation/201/2013-10704
     url(r'^regulation/(?P<reg_part>[\d]+)/%s$' % reg_version_pattern, 
         RegulationView.as_view(), 
         name='regulation_view'),
     #A regulation section without chrome
+    #Example: http://.../partial/201/2013-10704
     url(r'^partial/%s/%s$' % (part_section_pattern, reg_version_pattern), 
         PartialSectionView.as_view(), 
         name='partial_section_view'),
     #A regulation paragraph without chrome. 
+    #Example: http://.../partial/201-2-g/2013-10704
     url(r'^partial/(?P<paragraph_id>[-\d\w]+)/%s$' % reg_version_pattern,
         PartialParagraphView.as_view(),
         name='partial_regulation_view'),
