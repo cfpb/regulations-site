b89c58fde904bd24a3b75f7c41b8a0f9cb3aaf43
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-08-01 19:47:59 +0000

    Added a views utils test

---
+++ regserver/regulations/tests/views_utils_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from mock import Mock, patch
+
+from regulations.views.utils import *
+
+class UtilsTest(TestCase):
+    @patch('regulations.generator.generator.LayerCreator.get_layer_json')
+    def test_handle_specified_layers(self, get_layer_json):
+        get_layer_json.return_value = {'layer'}
+
+        layer_names = 'graphics,meta'
+        appliers = handle_specified_layers(layer_names, '205', '2013-1')
+        self.assertEquals(3, len(appliers))
+
