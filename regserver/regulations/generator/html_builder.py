ada62ad82e5f006e353849449173103db1f6bf82
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-08-01 19:40:55 +0000

    Fixed some variable weirdness with the html_builder, and added a partial section test

--- regserver/regulations/generator/html_builder.py
+++ regserver/regulations/generator/html_builder.py
@@ -43,7 +43,7 @@ class HTMLBuilder():
         elif node_type == 'appendix':
             level_type_map = {1:'a', 2:'1', 3:'i', 4:'A'}
             prefix_length = 3
-        elif node_type == 'regulation':
+        else:
             level_type_map = {1:'a', 2:'1', 3:'i', 4:'A'}
             prefix_length = 2
 
@@ -87,6 +87,7 @@ class HTMLBuilder():
         node['tree_level'] = len(node['label']['parts']) - 1
 
         node['node_type'] = self.node_type(node['tree_level'], node['label']['parts'])
+        print node['node_type']
         list_level, list_type = self.list_level(node['label']['parts'], node['node_type'])
 
         node['list_level'] = list_level
--- regserver/regulations/tests/partial_view_tests.py
+++ regserver/regulations/tests/partial_view_tests.py
@@ -24,3 +24,27 @@ class PartialParagraphViewTests(TestCase):
         response = view(request, paragraph_id=paragraph_id, reg_version=reg_version)
         self.assertEqual(response.context_data['node'], 
             generator.get_tree_paragraph.return_value)
+
+
+class PartialSectionViewTests(TestCase):
+    @patch('regulations.views.partial.generator')
+    @patch('regulations.generator.generator.LayerCreator.get_layer_json')
+    def test_get_context_data(self, get_layer_json, generator):
+        get_layer_json.return_value = {'layer'}
+        generator.get_tree_paragraph.return_value = {
+            'text': 'Some Text',
+            'children': [],
+            'label': {'text': '205', 'parts': ['205']}
+        }
+
+        reg_part_section = '205'
+        reg_version = '2013-10608'
+
+        request = RequestFactory().get('/fake-path/?layers=meta')
+        view = PartialSectionView.as_view(template_name='regulation-content.html')
+
+        response = view(request, 
+                    reg_part_section=reg_part_section,
+                    reg_version=reg_version)
+        self.assertEqual(response.context_data['tree'], 
+            generator.get_tree_paragraph.return_value)
