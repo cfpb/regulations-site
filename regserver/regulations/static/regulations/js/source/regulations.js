cbdd60dc01b573aada3f489e9cb137501d4c9633
Theresa Summa <theresa.summa@cfpb.gov>
2013-08-05 18:11:57 -0300

    my measly accessibility tests

--- regserver/regulations/static/regulations/js/source/regulations.js
+++ regserver/regulations/static/regulations/js/source/regulations.js
@@ -5,4 +5,17 @@ require(['jquery', 'app-init'], function($, app) {
         app.init();
     });
 
+    window.AccessibilityTest = function() {
+        $('img').each(function(i, val) {
+            if (typeof this.attributes.alt === 'undefined') {
+                console.error('Image does not have alt text', this);
+            }
+        });
+
+        $('p, h1, h2, h3, h4, h5, h6, div, span').each(function(i, val) {
+            if ($(this).css('font-size').indexOf('em') === -1 && $(this).css('font-size').indexOf('px') > 0) {
+                console.error('Font size is set in px, not ems', this);
+            }
+        });
+    };
 });
