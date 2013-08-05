a35feaefeff82ca192bc51e396a23996b75593e2
CM Lubinski <cm.lubinski@gmail.com>
2013-08-05 21:29:23 +0200

    Stream line partial views. Add partial view for interpretations

--- regserver/regserver/urls.py
+++ regserver/regserver/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from regulations.views.chrome import RegulationSectionView, RegulationView
-from regulations.views.partial import PartialSectionView, PartialParagraphView
+from regulations.views.partial import *
 
 #Re-usable URL patterns. 
 reg_version_pattern = r'(?P<reg_version>[-\d\w]+)'
@@ -18,14 +18,20 @@ urlpatterns = patterns('',
     url(r'^regulation/(?P<reg_part>[\d]+)/%s$' % reg_version_pattern, 
         RegulationView.as_view(), 
         name='regulation_view'),
+
     #A regulation section without chrome
     #Example: http://.../partial/201-4/2013-10704
-    url(r'^partial/%s/%s$' % (part_section_pattern, reg_version_pattern), 
+    url(r'^partial/(?P<label_id>[\d]+[-][\w]+)/%s$' % reg_version_pattern, 
         PartialSectionView.as_view(), 
         name='partial_section_view'),
+    #An interpretation of a section/paragraph or appendix without chrome.
+    #Example: http://.../partial/201-2-Interp/2013-10704
+    url(r'^partial/(?P<label_id>[-\d\w]+[-]Interp)/%s' % reg_version_pattern,
+        PartialInterpView.as_view(),
+        name='partial_interp_view'),
     #A regulation paragraph without chrome. 
     #Example: http://.../partial/201-2-g/2013-10704
-    url(r'^partial/(?P<paragraph_id>[-\d\w]+)/%s$' % reg_version_pattern,
+    url(r'^partial/(?P<label_id>[-\d\w]+)/%s$' % reg_version_pattern,
         PartialParagraphView.as_view(),
-        name='partial_regulation_view'),
+        name='partial_paragraph_view'),
 )
--- regserver/regulations/views/partial.py
+++ regserver/regulations/views/partial.py
@@ -5,49 +5,51 @@ from regulations.generator.html_builder import HTMLBuilder
 from regulations.views.chrome import RegulationSectionView, generate_html, build_context
 from regulations.views import utils
 
-class PartialSectionView(TemplateView):
-    template_name = 'regulation-content.html'
+class PartialView(TemplateView):
+    """Base class of various partial markup views"""
 
     def get_context_data(self, **kwargs):
-        context = super(PartialSectionView, self).get_context_data(**kwargs)
+        context = super(PartialView, self).get_context_data(**kwargs)
 
-        regulation_part = context['reg_part_section']
-        regulation_version = context['reg_version']
-
-        regulation = RegulationSectionView.get_regulation_part(context['reg_part_section'])
-
-        if 'layers' in self.request.GET.keys():
-            inline_applier, p_applier, s_applier = utils.handle_specified_layers(self.request.GET['layers'], 
-                    regulation_part, regulation_version, sectional=True)
-        else:
-            inline_applier, p_applier, s_applier = generator.get_all_section_layers(regulation_part, regulation_version)
-            inline_applier = generator.add_section_internal_citations(regulation, regulation_version, inline_applier)
-
-        section_tree = generator.get_tree_paragraph(regulation_part, regulation_version)
-        builder = generate_html(section_tree, (inline_applier, p_applier, s_applier))
-        context = build_context(context, builder)
-        return context
-        
-class PartialParagraphView(TemplateView):
-    """ Display a single paragraph of a regulation with all the chrome elements. """
-    template_name = "tree.html"
-
-    def get_context_data(self, **kwargs):
-
-        context = super(PartialParagraphView,
-                self).get_context_data(**kwargs)
-
-        paragraph_id = context['paragraph_id']
+        label_id = context['label_id']
         version = context['reg_version']
 
         if 'layers' in self.request.GET.keys():
             inline_applier, p_applier, s_applier = utils.handle_specified_layers(self.request.GET['layers'], 
-                paragraph_id, version, sectional=True)
+                    label_id, version, sectional=True)
         else:
-            inline_applier, p_applier, s_applier = generator.get_all_section_layers(paragraph_id, version)
-            inline_applier = generator.add_section_internal_citations(paragraph_id, version, inline_applier)
+            inline_applier, p_applier, s_applier = generator.get_all_section_layers(label_id, version)
+            inline_applier = generator.add_section_internal_citations(label_id, 
+                    version, inline_applier)
 
-        paragraph_tree = generator.get_tree_paragraph(paragraph_id, version)
-        builder = generate_html(paragraph_tree, (inline_applier, p_applier, s_applier))
+        tree = generator.get_tree_paragraph(label_id, version)
+        builder = generate_html(tree, (inline_applier, p_applier, s_applier))
+        return self.transform_context(context, builder)
+
+
+class PartialSectionView(PartialView):
+    """ Single section of reg text """
+    template_name = 'regulation_text.html'
+
+    def transform_context(self, context, builder):
+        context['c'] = builder.tree
+        return context
+
+        
+class PartialParagraphView(PartialView):
+    """ Single paragraph of a regtext """
+
+    template_name = "tree.html"
+
+    def transform_context(self, context, builder):
         context['node'] = builder.tree
+        return context
+
+class PartialInterpView(PartialView):
+    """ Interpretation of a reg text section/paragraph or appendix """
+
+    template_name = "interp-tree.html"
+
+    def transform_context(self, context, builder):
+        context['interp'] = builder.tree
         return context
