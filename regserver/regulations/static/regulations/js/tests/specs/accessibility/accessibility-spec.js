dbb02655eeb0d09bd3459bb0df203fc96a0d22e8
Theresa Summa <theresa.summa@cfpb.gov>
2013-08-13 19:30:35 -0300

    removing files that were never supposed to make it in

--- regserver/regulations/static/regulations/js/tests/specs/accessibility/accessibility-spec.js
+++
@@ -1,6 +0,0 @@
-describe('full app accessibility', function() {
-console.log('hi');
-    it('should have started the app', function() {
-        expect(app.hasOwnProperty('getTree')).toBeTruthy();
-    });
-});     
