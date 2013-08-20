dbae83d24fb546c2d09aaa1bfcfa3c9f995d1dc5
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-08-20 19:50:04 +0000

    PEP8 changes

--- regserver/regulations/tests/title_parsing_tests.py
+++ regserver/regulations/tests/title_parsing_tests.py
@@ -4,7 +4,9 @@ from regulations.generator import title_parsing
 
 class RegTest(TestCase):
     def test_try_split(self):
-        self.assertEqual(['a', 'xb'], title_parsing.try_split('a:xb', ('|', ':', 'x')))
+        self.assertEqual(
+            ['a', 'xb'],
+            title_parsing.try_split('a:xb', ('|', ':', 'x')))
 
     def test_appendix_supplement_ap(self):
         elements = title_parsing.appendix_supplement({
@@ -14,7 +16,7 @@ class RegTest(TestCase):
 
     def test_section(self):
         elements = title_parsing.section({
-            'index': ['204', '4'], 
+            'index': ['204', '4'],
             'title': '204.4 Sauce'})
 
         self.assertTrue(elements['is_section'])
