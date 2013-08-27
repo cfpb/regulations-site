6113641eaefb8f2571f40ddccee80c682a194451
CM Lubinski <cm.lubinski@gmail.com>
2013-08-27 22:27:04 +0200

    Update test

--- regserver/regulations/tests/layers_analyses_tests.py
+++ regserver/regulations/tests/layers_analyses_tests.py
@@ -6,11 +6,11 @@ class SectionBySectionLayerTest(TestCase):
     def test_apply_layer(self):
         layer = {
             "111-22": [{
-                "reference": ["2009-22", "111-22"],
-                "text": "Newer analysis"
-            }, {
-                "reference": ["2007-11", "111-22"],
+                "reference": ["2007-22", "111-22"],
                 "text": "Older analysis"
+            }, {
+                "reference": ["2009-11", "111-22"],
+                "text": "Newer analysis"
             }],
             "111-22-a": [{
                 "reference": ["2009-22", "111-22-a"],
@@ -21,10 +21,14 @@ class SectionBySectionLayerTest(TestCase):
         
         key, value = sxs.apply_layer("111-22")
         self.assertEqual("analyses", key)
-        self.assertEqual([["2009-22", "111-22"], ["2007-11", "111-22"]], value)
+        self.assertEqual([
+            {'doc_number': '2009-11', 'label_id': '111-22', 'text': '111.22'},
+            {'doc_number': '2009-22', 'label_id': '111-22-a',
+             'text': '111.22(a)'}], value)
 
         key, value = sxs.apply_layer("111-22-a")
         self.assertEqual("analyses", key)
-        self.assertEqual([["2009-22", "111-22-a"]], value)
+        self.assertEqual([{'doc_number': '2009-22', 'label_id': '111-22-a',
+                           'text': '111.22(a)'}], value)
 
         self.assertEqual(None, sxs.apply_layer("222-22"))
