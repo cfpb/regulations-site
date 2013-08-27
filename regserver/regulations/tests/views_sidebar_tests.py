9f83315c1be0268836a3a10fade9ba832d614e79
CM Lubinski <cm.lubinski@gmail.com>
2013-08-27 23:40:05 +0200

    Use a 'valid' doc number

--- regserver/regulations/tests/views_sidebar_tests.py
+++ regserver/regulations/tests/views_sidebar_tests.py
@@ -12,11 +12,15 @@ class ViewsSideBarViewTest(TestCase):
     def test_get(self, api_reader):
         api_reader.Client.return_value.layer.return_value = {
             '1111-1': [
-                {'reference': ['doc1', '1111-1']},
-                {'reference': ['doc2', '1111-1']},
+                {'reference': ['1992-1', '1111-1']},
+                {'reference': ['1992-2', '1111-1']},
             ],
-            '1111-1-a': [{'reference': ['doc1', '1111-1-a']}]
+            '1111-1-a': [{'reference': ['1992-1', '1111-1-a']}]
         }
         response = Client().get('/partial/sidebar/1111-1/verver')
         self.assertTrue(bool(re.search(r'\b1111\.1\b', response.content)))
         self.assertTrue('1111.1(a)' in response.content)
+        self.assertTrue('1992-2' in response.content)
+        self.assertTrue('1111-1' in response.content)
+        self.assertTrue('1992-1' in response.content)
+        self.assertTrue('1111-1-a' in response.content)
