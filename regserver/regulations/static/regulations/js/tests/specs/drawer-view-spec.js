915a83952f6d5e93f958ae911bdbb9b8748f7ad4
Theresa Summa <theresa.summa@cfpb.gov>
2013-08-28 13:29:06 -0300

    adding tests for drawer view

---
+++ regserver/regulations/static/regulations/js/tests/specs/drawer-view-spec.js
@@ -0,0 +1,30 @@
+define(['jquery', 'underscore', 'backbone', 'drawer-view'], function($, _, Backbone, DrawerView) {
+    describe("Drawer view", function() {
+        var drawerView = new DrawerView(),
+            drawerView2; 
+
+        it("should default to having the TOC active", function() {
+            expect(drawerView.childViews['table-of-contents'].view).toBeTruthy();
+        });
+
+        it("should hide all drawer panes besides TOC", function() {
+            $('.toc-container:not(#table-of-contents)').each(function(el) {
+                expect($(el).hasClass('hidden')).toBeTruthy();
+            });
+        });
+
+        it("should instantiate the history view if the cookie is set", function() {
+            drawerView.changeContents('history');
+            drawerView2 = new DrawerView();
+
+            expect(drawerView2.childViews['history'].view).toBeTruthy();            
+        });
+
+        it("should hide all drawer panes besides history", function() {
+            $('.toc-container:not(#history)').each(function(el) {
+                expect($(el).hasClass('hidden')).toBeTruthy();
+            });
+        });
+    });
+});
+
