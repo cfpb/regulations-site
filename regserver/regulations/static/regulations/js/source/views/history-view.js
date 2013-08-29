b53a311d1bb1c63c35fc148b1d756048da2a00b0
Adam Scott <adam.scott@cfpb.gov>
2013-08-29 17:11:19 -0400

    less verbose history ui

--- regserver/regulations/static/regulations/js/source/views/history-view.js
+++ regserver/regulations/static/regulations/js/source/views/history-view.js
@@ -6,15 +6,11 @@ define('history-view', ['jquery', 'underscore', 'backbone', 'dispatch'], functio
         el: '#history',
 
        initialize: function() {
-            var $statusList = $('.status-list');
+            // remove the current class from all .status-list items
+            $('.status-list').removeClass('current');
+            
             // check the data-base-version attribute of each <li> against the document
-            $statusList.each(function() {
-                if ($(this).data('base-version') === Dispatch.getVersion()) {
-                    $statusList.removeClass('current');
-                    // for the matching <li> add a class of 'current'
-                    $(this).addClass('current');
-                }
-            });
+            this.$el.find('.status-list[data-base-version=' + Dispatch.getVersion() + ']').addClass('current');
         }
 
 
