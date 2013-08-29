40da567dcd6eae1ab777eb0640a726dba801d7b8
Adam Scott <adam.scott@cfpb.gov>
2013-08-29 16:58:07 -0400

    set cookie to expire after 60 minutes

--- regserver/regulations/static/regulations/js/source/views/drawer-view.js
+++ regserver/regulations/static/regulations/js/source/views/drawer-view.js
@@ -12,7 +12,7 @@ define('drawer-view', ['jquery', 'underscore', 'backbone', 'jquery-cookie', 'dis
         initialize: function() {
             // the cookieValue defaults to the table of contents, else the value of the cookie set within changeContents
             var cookieValue = $.cookie('Drawer_State') || 'table-of-contents';
-
+            
             Dispatch.on('drawer:stateChange', this.changeContents, this);
 
             this.$label = $('.toc-type');
@@ -40,6 +40,10 @@ define('drawer-view', ['jquery', 'underscore', 'backbone', 'jquery-cookie', 'dis
         },
 
         changeContents: function(activeId) {
+            var $expiration = new Date(),
+                minutes = 60;
+            //set date to 60 minutes for cookie expiration    
+            $expiration.setTime($expiration.getTime() + (minutes * 60 * 1000));
             // hide the content of all drawer sections
             this.$children.addClass('hidden');
             // remove the 'hidden' class from the active drawer section
@@ -51,7 +55,7 @@ define('drawer-view', ['jquery', 'underscore', 'backbone', 'jquery-cookie', 'dis
             Dispatch.set('drawerState', activeId);
 
             // set a cookie value equal to the active drawer id
-            $.cookie('Drawer_State', activeId);
+            $.cookie('Drawer_State', activeId, { expires: $expiration });
         }
 
     });
