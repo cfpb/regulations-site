a25199546bbbe0e605330b559c3c8405f5b10e4a
Theresa Summa <theresa.summa@cfpb.gov>
2013-09-05 15:06:56 -0300

    more flexible routing structure

--- regserver/regulations/static/regulations/js/source/regs-router.js
+++ regserver/regulations/static/regulations/js/source/regs-router.js
@@ -9,17 +9,19 @@ define('regs-router', ['underscore', 'backbone', './dispatch'], function(_, Back
         backToSection: function(section) {
             Dispatch.trigger('openSection:set', section); 
             Dispatch.trigger('sxs:close');
+        },
+
+        start:  function() {
+            var root = Dispatch.getURLPrefix() || '/';
+
+            Backbone.history.start({
+                pushState: 'pushState' in window.history,
+                silent: true,
+                root: root
+            });
         }
     });
 
-    var router = new RegsRouter(),
-        root = Dispatch.getURLPrefix() || '/';
-
-    Backbone.history.start({
-        pushState: 'pushState' in window.history,
-        silent: true,
-        root: root
-    });
-
+    var router = new RegsRouter();
     return router;
 });
