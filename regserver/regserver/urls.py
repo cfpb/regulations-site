90506789edab1afb58ecebb90218f2654498a754
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-09-16 16:14:52 +0000

    Remove eregulations url as reversing is not consistent

--- regserver/regserver/urls.py
+++ regserver/regserver/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     '',
     url(r'', include('regulations.urls')),
-    url(r'^eregulations/', include('regulations.urls')),
 )
