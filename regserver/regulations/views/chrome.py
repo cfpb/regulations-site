a6aad52e9bc6724b268b350abd9a72b1d6d82c11
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-09-18 11:28:15 +0000

    Removed unused import

--- regserver/regulations/views/chrome.py
+++ regserver/regulations/views/chrome.py
@@ -1,7 +1,6 @@
 from datetime import date
 
 from django.conf import settings
-from django.http import Http404
 from django.views.generic.base import TemplateView
 
 from regulations.generator import generator
