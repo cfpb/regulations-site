852f93f15cd95542a5bdffd05512ba6013a293e3
CM Lubinski <cm.lubinski@gmail.com>
2013-09-23 01:08:21 +0200

    Update history links when section changes

--- regserver/regulations/static/regulations/js/source/views/history-view.js
+++ regserver/regulations/static/regulations/js/source/views/history-view.js
@@ -10,15 +10,29 @@ define('history-view', ['jquery', 'underscore', 'backbone', 'dispatch'], functio
         },
 
         initialize: function() {
+            Dispatch.on('regSection:open:after', this.updateLinks, this);
+
             // remove the current class from all .status-list items
             $('.status-list').removeClass('current');
             
             // check the data-base-version attribute of each <li> against the document
             this.$el.find('.status-list[data-base-version=' + Dispatch.getVersion() + ']').addClass('current');
+
+            //  History view may not have been initialized before the section was updated;
+            //  update the links now.
+            this.updateLinks();
         },
 
         setStorageItem: function() {
             sessionStorage.setItem('drawerDefault', 'history');
+        },
+
+        updateLinks: function() {
+            var currentSection = Dispatch.getOpenSection();
+            this.$el.find('.version-link').each(function() {
+                var $link = $(this);
+                $link.attr('href', window.APP_PREFIX + currentSection + '/' + $link.data('version'));
+            });
         }
     });
 
