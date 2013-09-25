0d9ef4cef4bca9a0454e594b4857cf46c252299a
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-09-25 19:08:51 +0000

    Fixed test

--- regserver/regulations/tests/views_chrome_tests.py
+++ regserver/regulations/tests/views_chrome_tests.py
@@ -5,15 +5,20 @@ from django.test import RequestFactory
 from mock import Mock, patch
 
 from regulations.views.chrome import *
+from regulations.views.error_handling import MissingContentException
 
 
 class ViewsChromeTest(TestCase):
+    @patch('regulations.views.error_handling.api_reader')
+    @patch('regulations.views.chrome.ChromeView.set_chrome_context')
     @patch('regulations.views.chrome.generator')
-    def test_404(self, generator):
+    def test_404(self, generator, set_chrome_context, api_reader):
         """Test that the response of the outer view is that of the inner
         when there's an error"""
+        api_reader.ApiReader.return_value.regversions.return_value = None
         generator.get_regulation.return_value = None
         generator.get_tree_paragraph.return_value = {}
+        set_chrome_context.return_value = None
 
         class InnerView(TemplateView):
             def get(self, request, *args, **kwargs):
@@ -30,12 +35,12 @@ class ViewsChromeTest(TestCase):
     @patch('regulations.views.chrome.error_handling')
     @patch('regulations.views.chrome.ChromeView.set_chrome_context')
     @patch('regulations.views.chrome.generator')
-    def test_error_propagation(self, generator, set_tree_context, error_handling):
+    def test_error_propagation(self, generator, set_chrome_context, error_handling):
         """Test that the response of the outer view is that of the inner
         when there's an error"""
         error_handling.check_regulation.return_value = None
         generator.get_tree_paragraph.return_value = {}
-        set_tree_context.return_value = None
+        set_chrome_context.return_value = None
 
         class InnerView(TemplateView):
             def get(self, request, *args, **kwargs):
