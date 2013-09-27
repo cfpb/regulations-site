4069a49e482863ca3c71f928db2961788386318c
CM Lubinski <cm.lubinski@gmail.com>
2013-09-27 23:53:01 +0200

    Template tag for changing context

---
+++ regserver/regulations/templatetags/in_context.py
@@ -0,0 +1,37 @@
+from django import template
+
+register = template.Library()
+
+class InContextNode(template.Node):
+    def __init__(self, nodelist, subcontext_names):
+        self.nodelist = nodelist
+        self.subcontext_names = subcontext_names
+    
+    def render(self, context):
+        new_context = {}
+        for field in self.subcontext_names:
+            new_context.update(context.get(field, {}))
+        return self.nodelist.render(template.Context(new_context))
+
+@register.tag('begincontext')
+def in_context(parser, token):
+    """
+    Replaces the context (inside of this block) for easy (and safe) inclusion
+    of sub-content.
+
+    For example, if the context is {'name': 'Kitty', 'sub': {'size': 5}}
+
+        1: {{ name }} {{ size }}
+        {% begincontext sub %}
+        2: {{ name }} {{ size }}
+        {% endcontext %}
+        3: {{ name }} {{ size }}
+
+    Will print
+        1: Kitty
+        2:  5
+        3: Kitty
+    """
+    nodelist = parser.parse(('endcontext',))
+    parser.delete_first_token()
+    return InContextNode(nodelist, token.split_contents()[1:])
