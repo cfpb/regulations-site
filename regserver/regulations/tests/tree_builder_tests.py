5ca4225a2b19270964beb87518d79329caa05604
CM Lubinski <cm.lubinski@gmail.com>
2013-10-03 01:39:58 +0200

    PEP 8

--- regserver/regulations/tests/tree_builder_tests.py
+++ regserver/regulations/tests/tree_builder_tests.py
@@ -4,6 +4,7 @@ from regulations.generator.node_types import REGTEXT
 
 import itertools
 
+
 class TreeBuilderTest(TestCase):
 
     def build_tree(self):
@@ -11,11 +12,11 @@ class TreeBuilderTest(TestCase):
             'text': 'child text',
             'children': [],
             'label_id': '204-3',
-            'label': ['204','3'],
+            'label': ['204', '3'],
             'node_type': REGTEXT
         }
         tree = {
-            'text':'parent text', 
+            'text': 'parent text',
             'children': [child],
             'label_id': '204',
             'label': ['204'],
@@ -24,7 +25,6 @@ class TreeBuilderTest(TestCase):
         return tree
 
     def test_parent_in_tree(self):
-    
         tree = self.build_tree()
         tree_hash = tree_builder.build_tree_hash(tree)
         self.assertEqual(tree_hash.keys(), ['204-3', '204'])
@@ -36,7 +36,7 @@ class TreeBuilderTest(TestCase):
             'text': 'new node text',
             'children': [],
             'label_id': '204-4',
-            'label': ['204','4'],
+            'label': ['204', '4'],
             'node_type': REGTEXT
         }
         tree = self.build_tree()
@@ -46,7 +46,7 @@ class TreeBuilderTest(TestCase):
         self.assertEqual(len(tree_hash['204']['children']), 1)
         tree_builder.add_node_to_tree(new_node, '204', tree_hash)
         self.assertEqual(len(tree_hash['204']['children']), 2)
-        
+
         child_labels = [c['label_id'] for c in tree_hash['204']['children']]
         self.assertEqual(child_labels, ['204-3', '204-4'])
 
@@ -96,7 +96,7 @@ class TreeBuilderTest(TestCase):
 
         child = {
             'children': [],
-            'label': ['204','2'],
+            'label': ['204', '2'],
             'label_id': '204-2',
             'node_type': REGTEXT,
             'sortable': 2,
@@ -105,7 +105,7 @@ class TreeBuilderTest(TestCase):
 
         static_child = {
             'children': [],
-            'label': ['204','3'],
+            'label': ['204', '3'],
             'label_id': '204-3',
             'node_type': REGTEXT,
             'sortable': 3,
@@ -114,7 +114,7 @@ class TreeBuilderTest(TestCase):
 
         static_tree = {
             'children': [child, static_child],
-            'text':'parent text', 
+            'text': 'parent text',
             'label': ['204'],
             'label_id': '204',
             'node_type': REGTEXT
