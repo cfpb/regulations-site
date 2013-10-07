b86c94d3844027909a1213542bfa98f338e9c540
CM Lubinski <cm.lubinski@gmail.com>
2013-10-08 00:22:12 +0200

    Use EREGS_GA_* for google analytics settings, but be backwards compatible

--- regserver/regulations/tests/views_utils_test.py
+++ regserver/regulations/tests/views_utils_test.py
@@ -7,6 +7,35 @@ from regulations.views.utils import *
 
 
 class UtilsTest(TestCase):
+    def setUp(self):
+        if hasattr(settings, 'GOOGLE_ANALYTICS_ID'):
+            self.old_gai = settings.GOOGLE_ANALYTICS_ID
+        if hasattr(settings, 'GOOGLE_ANALYTICS_SITE'):
+            self.old_gas = settings.GOOGLE_ANALYTICS_SITE
+        if hasattr(settings, 'EREGS_GA_ID'):
+            self.eregs_gai = settings.EREGS_GA_ID
+        if hasattr(settings, 'EREGS_GA_SITE'):
+            self.eregs_gas = settings.EREGS_GA_SITE
+
+    def tearDown(self):
+        if hasattr(self, 'old_gai'):
+            settings.GOOGLE_ANALYTICS_ID = self.old_gai
+        elif hasattr(settings, 'GOOGLE_ANALYTICS_ID'):
+            del(settings.GOOGLE_ANALYTICS_ID)
+        if hasattr(self, 'old_has'):
+            settings.GOOGLE_ANALYTICS_SITE = self.old_gas
+        elif hasattr(settings, 'GOOGLE_ANALYTICS_SITE'):
+            del(settings.GOOGLE_ANALYTICS_SITE)
+
+        if hasattr(self, 'eregs_gai'):
+            settings.EREGS_GA_ID = self.eregs_gai
+        elif hasattr(settings, 'EREGS_GA_ID'):
+            del(settings.EREGS_GA_ID)
+        if hasattr(self, 'eregs_gas'):
+            settings.EREGS_GA_SITE = self.eregs_gas
+        elif hasattr(settings, 'EREGS_GA_SITE'):
+            del(settings.EREGS_GA_SITE)
+
     def test_get_layer_list(self):
         names = 'meta,meta,GRAPHICS,fakelayer,internal'
         layer_list = get_layer_list(names)
@@ -39,6 +68,18 @@ class UtilsTest(TestCase):
         self.assertTrue('GOOGLE_ANALYTICS_SITE' in context)
         self.assertTrue('APP_PREFIX' in context)
         self.assertTrue('env' in context)
+
+    def test_add_extras_gai(self):
+        """Make sure we are backwards compatible with GOOGLE_* params"""
+        settings.GOOGLE_ANALYTICS_ID = 'googid'
+        settings.GOOGLE_ANALYTICS_SITE = 'googsite'
+        del(settings.EREGS_GA_ID)
+        del(settings.EREGS_GA_SITE)
+
+        context = {}
+        add_extras(context)
+        self.assertEqual('googid', context['GOOGLE_ANALYTICS_ID'])
+        self.assertEqual('googsite', context['GOOGLE_ANALYTICS_SITE'])
 
     @patch('regulations.views.utils.table_of_contents')
     def test_first_section(self, table_of_contents):
