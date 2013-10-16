8efa550694c7c8b799989b6c8764340ce2c72062
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-10-16 21:02:07 +0000

    Removed version line from search results, included effective date on search results not found page, changed drawer search text

--- regserver/regulations/tests/views_partial_search_tests.py
+++ regserver/regulations/tests/views_partial_search_tests.py
@@ -40,8 +40,23 @@ class PartialSearchTest(TestCase):
         self.assertTrue('111.25' in response.content)
         self.assertTrue('3333' in response.content)
 
-        # Version info
-        self.assertTrue('timeytimey' in response.content.lower())
+    @patch('regulations.views.partial_search.api_reader')
+    @patch('regulations.views.partial_search.fetch_grouped_history')
+    def test_get_no_results(self, fetch_grouped_history, api_reader):
+        api_reader.ApiReader.return_value.search.return_value = {
+            'total_hits': 0,
+            'results': []
+        }
+        fetch_grouped_history.return_value = [
+            {'notices': [{'document_number': 'bbb',
+                          'effective_on': date(2012, 12, 12)}]},
+            {'notices': [{'document_number': 'ccc',
+                          'effective_on': date(2001, 1, 1)},
+                         {'document_number': 'vvv',
+                          'effective_on': date(2003, 4, 5)}],
+             'timeline': 'timeytimey'}
+        ]
+        response = Client().get('/partial/search/111?version=vvv&q=none')
         self.assertTrue('4/5/2003' in response.content)
 
     def test_get_400(self):
