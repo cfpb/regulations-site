4f428c32d4e3c6d96a47b2ebb4226d5b9bcfec91
CM Lubinski <cm.lubinski@gmail.com>
2013-10-18 00:40:30 +0200

    Separate defined from definitions layer. Use defined in diffs

---
+++ regserver/regulations/generator/layers/defined.py
@@ -0,0 +1,23 @@
+from django.template import loader, Context
+
+
+class DefinedLayer(object):
+    shorthand = 'defined'
+
+    def __init__(self, layer):
+        self.layer = layer
+        self.template = loader.get_template('layers/defining.html')
+
+    def apply_layer(self, text, text_index):
+        """Catch all terms which are defined in this paragraph, replace them
+        with a span"""
+        layer_pairs = []
+        for ref_struct in self.layer['referenced'].values():
+            if text_index == ref_struct['reference']:
+                pos = tuple(ref_struct['position'])
+                original = text[pos[0]:pos[1]]
+                context = Context({'term': original})
+                replacement = self.template.render(context)
+                replacement = replacement.strip('\n')
+                layer_pairs.append((original, replacement, pos))
+        return layer_pairs
