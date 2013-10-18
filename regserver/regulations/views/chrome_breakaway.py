f3ecae04fa5a2e30cb627c054afdb134863f758b
CM Lubinski <cm.lubinski@gmail.com>
2013-10-18 20:55:50 +0200

    Add a listing of other analyses to SxS rhs. Make sure version is present in the breakaway template

--- regserver/regulations/views/chrome_breakaway.py
+++ regserver/regulations/views/chrome_breakaway.py
@@ -20,8 +20,9 @@ class ChromeBreakawayView(ChromeView):
         context = super(ChromeView, self).get_context_data(**kwargs)
 
         context['regulation'] = context['label_id'].split('-')[0]
+        context['version'] = self.request.GET.get('from_version')
         meta = api_reader.ApiReader().layer('meta',
-            context['regulation'], self.request.GET.get('from_version'))
+            context['regulation'], context['version'])
         context['meta'] = meta[context['regulation']][0]
 
         content = self.content(context)
