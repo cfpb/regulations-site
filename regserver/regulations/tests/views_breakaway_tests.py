9d7686b31cf2b064c4d151657c859c8aeb2194b2
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-10-18 19:46:02 +0000

    Adding the regulation part to the page title

---
+++ regserver/regulations/tests/views_breakaway_tests.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from mock import patch
+from django.test import RequestFactory
+from django.test.client import Client
+
+from regulations.views.chrome_breakaway import *
+
+class ChromeSXSViewTests(TestCase):
+
+    @patch('regulations.views.chrome_breakaway.api_reader')
+    @patch('regulations.views.chrome_breakaway.ChromeSXSView.content')
+    def test_get(self, content, api_reader):
+        content.return_value = ''
+        api_reader.ApiReader.return_value.layer.return_value = {
+            '204':[''],
+        }
+        request = RequestFactory().get('/fake-path/204-2/2013-1?from_version=2014-2')
+
+        view = ChromeSXSView.as_view(template_name='breakaway-chrome.html')
+        response = view(
+            request, label_id='204-2', from_version='2014-2', notice_id='2013-1')
+
+        self.assertEqual(response.context_data['reg_part'], '204')
