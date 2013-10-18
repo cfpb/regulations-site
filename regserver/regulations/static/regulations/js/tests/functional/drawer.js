6503f3f46bc05d24e05906fc76ed9f9ebf99be32
Theresa Summa <theresa.summa@cfpb.gov>
2013-10-17 23:11:34 -0300

    fleshing out drawer tests

--- regserver/regulations/static/regulations/js/tests/functional/drawer.js
+++ regserver/regulations/static/regulations/js/tests/functional/drawer.js
@@ -7,26 +7,55 @@ casper.start();
 casper.thenOpen(url + '/1005', function() {
 });
 
+// all drawer tabs are present
 casper.waitForSelector('#site-header', function() {
     this.test.assertExists(
-        '#table-of-contents',
+        '#menu-link',
         'TOC drawer tab is present'
     );
 
     this.test.assertExists(
-        '#timeline',
+        '#timeline-link',
         'Timeline drawer tab is present'
     );
 
     this.test.assertExists(
-        '#search',
+        '#search-link',
         'Search drawer tab is present'
     );
-
 });
 
 casper.then(function() {
-    this.click('#timeline');
+    // activate timeline pane
+    this.click('#timeline-link');
+});
+
+casper.wait(5000);
+
+// timeline pane should be showing
+casper.then(function() {
+    this.test.assertExists(
+        '#table-of-contents.hidden',
+        'TOC pane is hidden'
+    );
+
+    this.test.assertExists(
+        '#search.hidden',
+        'Search pane is hidden'
+    );
+
+    this.test.assertDoesntExist(
+        '#timeline.hidden',
+        'Timeline pane is visible'
+    );
+});
+
+// make sure timeline tab is highlighted
+casper.then(function() {
+    this.test.assertExists(
+        '#timeline-link.current',
+        'Timeline drawer tab is highlighted'
+    );
 });
 
 casper.run(function() {
