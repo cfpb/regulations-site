7123111c1b46499ecfd7fd9a369e806422c2d5a4
Theresa Summa <theresa.summa@cfpb.gov>
2013-10-24 17:34:14 -0200

    basic static reorg and require cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -126,48 +126,8 @@ module.exports = function(grunt) {
                 mainConfigFile: '<%= env.frontEndPath %>/js/source/build.js',
                 dir: "<%= env.frontEndPath %>/js/built",
                 modules: [ {name: "regulations"} ],
-                paths: {
-                    jquery: './lib/jquery-1.9.1',
-                    underscore: './lib/underscore',
-                    backbone: './lib/backbone',
-                    'queryparams': './lib/backbone.queryparams',
-                    'jquery-scrollstop': './lib/jquery.scrollstop',
-                    'definition-view': './views/definition-view',
-                    'interpretation-view': './views/interpretation-view',
-                    'regs-fixed-el-view': './views/regs-fixed-el-view',
-                    'sub-head-view': './views/sub-head-view',
-                    'sidebar-module-view': './views/sidebar-module-view',
-                    'toc-view': './views/toc-view',
-                    'sidebar-view': './views/sidebar-view',
-                    'sidebar-head-view': './views/sidebar-head-view',
-                    'reg-view': './views/reg-view',
-                    'konami': './lib/konami',
-                    'analytics-handler': './views/analytics-handler-view',
-                    'header-view': './views/header-view',
-                    'section-footer-view': './views/section-footer-view',
-                    'drawer-view': './views/drawer-view',
-                    'history-view': './views/history-view',
-                    'search-view': './views/search-view',
-                    'sxs-list-view': './views/sxs-list-view',
-                    'sidebar-list-view': './views/sidebar-list-view',
-                    'sxs-view': './views/sxs-view',
-                    'search-results-view': './views/search-results-view',
-                    'main-view': './views/main-view',
-                    'permalink-view': './views/permalink-view'
-                },
-                shim: {
-                    underscore: {
-                        deps: ['jquery'],
-                        exports: '_'
-                    },
-                    backbone: {
-                        deps: ['underscore', 'jquery'],
-                        exports: 'Backbone'
-                    },
-                    konami: {
-                        exports: 'Konami'
-                    }
-                }
+                paths: grunt.file.readJSON('require.paths.json'),
+                shim: grunt.file.readJSON('require.shim.json')
             }
         }
     },
