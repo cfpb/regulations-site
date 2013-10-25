9fd12dbc1e1da9c964dcec921de9da4b4b104f0b
Theresa Summa <theresa.summa@cfpb.gov>
2013-10-25 13:04:48 -0200

    script shouldn't error if require conf doesn't exist

--- require.sh
+++ require.sh
@@ -1,4 +1,4 @@
-rm regserver/regulations/static/regulations/js/source/require.config.js
+rm -f regserver/regulations/static/regulations/js/source/require.config.js
 echo 'var require = { "paths" :' >> regserver/regulations/static/regulations/js/source/require.config.js
 cat require.paths.json >> regserver/regulations/static/regulations/js/source/require.config.js
 echo ', "shim":' >> regserver/regulations/static/regulations/js/source/require.config.js
