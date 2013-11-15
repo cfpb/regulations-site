1c1a1b10ff319981d8997227513a8c69f53ee907
Theresa Summa <theresa.summa@cfpb.gov>
2013-11-15 18:38:45 -0200

    try to get a test to run

--- regulations/tests/selenium/example_test.py
+++ regulations/tests/selenium/example_test.py
@@ -1,12 +1,16 @@
 import os
 import unittest
+import base64
+import json
+import httplib
+import sys
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 class ExampleTest(unittest.TestCase):
 
     def setUp(self):
-        if (os.environ.get('TRAVIS_SECURE_ENV_VARS')):
+        if (os.environ.get('TRAVIS')):
             self.capabilities['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
             self.capabilities['build'] = os.environ['TRAVIS_BUILD_NUMBER']
             self.capabilities['browserName'] = 'chrome'
@@ -22,6 +26,18 @@ class ExampleTest(unittest.TestCase):
         self.jobid = self.driver.session_id
         print "Sauce Labs job: https://saucelabs.com/jobs/%s" % self.jobid
         self.driver.implicitly_wait(30)
+
+    def report_test_result(self):
+        base64string = base64.encodestring('%s:%s'
+                                           % (self.username, self.key))[:-1]
+        result = json.dumps({'passed': sys.exc_info() == (None, None, None)})
+        connection = httplib.HTTPConnection("saucelabs.com")
+        connection.request('PUT',
+                           '/rest/v1/%s/jobs/%s' % (self.username, self.jobid),
+                           result,
+                           headers={"Authorization": "Basic %s" % base64string})
+        result = connection.getresponse()
+        return result.status == 200
 
     def example_test(self):
         self.driver.get(self.url)
