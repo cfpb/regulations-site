8dced8d783ab291016c567108e777bd9e1fca1e0
Theresa Summa <theresa.summa@cfpb.gov>
2013-11-15 17:07:35 -0200

    run selenium post pull request

--- regulations/tests/selenium/example_test.py
+++ regulations/tests/selenium/example_test.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 class ExampleTest(unittest.TestCase):
 
     def setUp(self):
-        if (os.environ.get('TRAVIS')):
+        if (os.environ.get('TRAVIS_SECURE_ENV_VARS'):
             self.capabilities['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
             self.capabilities['build'] = os.environ['TRAVIS_BUILD_NUMBER']
             self.capabilities['browserName'] = 'chrome'
@@ -16,7 +16,7 @@ class ExampleTest(unittest.TestCase):
         self.url = 'http://localhost:8000'
         self.username = os.environ['SAUCE_USERNAME']
         self.key = os.environ['SAUCE_ACCESS_KEY']
-        hub_url = 'http://username:access_key@ondemand.saucelabs.com/wd/hub'
+        hub_url = "%s%s@localhost:4445" % (self.username, self.key)
         self.driver = webdriver.Remote(desired_capabilities=self.capabilities,
                                        command_executor = "http://%s/wd/hub" % hub_url)
         self.jobid = self.driver.session_id
