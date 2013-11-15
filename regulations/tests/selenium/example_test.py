27d6f18d64acefc4fa3e29e37a4bf401ed0c44a6
Theresa Summa <theresa.summa@cfpb.gov>
2013-11-15 19:29:12 -0200

    try to get a test to run

--- regulations/tests/selenium/example_test.py
+++ regulations/tests/selenium/example_test.py
@@ -20,9 +20,9 @@ class ExampleTest(unittest.TestCase):
         self.url = 'http://localhost:8000'
         self.username = os.environ['SAUCE_USERNAME']
         self.key = os.environ['SAUCE_ACCESS_KEY']
-        hub_url = "%s%s@localhost:4445" % (self.username, self.key)
+        hub_url = "%s:%s" % (self.username, self.key)
         self.driver = webdriver.Remote(desired_capabilities=self.capabilities,
-                                       command_executor = "http://%s/wd/hub" % hub_url)
+                                       command_executor = "http://%s@ondemand.saucelabs.com:80/wd/hub" % hub_url)
         self.jobid = self.driver.session_id
         print "Sauce Labs job: https://saucelabs.com/jobs/%s" % self.jobid
         self.driver.implicitly_wait(30)
