8ad5643fa8fa878b7e773ca9912349aaec96361f
Theresa Summa <theresa.summa@cfpb.gov>
2013-11-18 17:49:01 -0200

    abstract selenium test set up/tear down, add def test, move tests

--- regulations/uitests/toc_test.py
+++ regulations/uitests/toc_test.py
@@ -1,35 +1,18 @@
 import os
 import unittest
-import base64
-import json
-import httplib
-import sys
+from base_test import BaseTest
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-class ExampleTest(unittest.TestCase):
+class TOCTest(BaseTest, unittest.TestCase):
 
-    def setUp(self):
-        self.capabilities = webdriver.DesiredCapabilities.CHROME
-        if os.environ.get('TRAVIS') and os.environ.get('TRAVIS_SECURE_ENV_VARS'):
-            self.capabilities['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
-            self.capabilities['build'] = os.environ['TRAVIS_BUILD_NUMBER']
-            self.username = os.environ['SAUCE_USERNAME']
-            self.key = os.environ['SAUCE_ACCESS_KEY']
-
-        self.capabilities['platform'] = 'LINUX'
-        self.capabilities['version'] = ''
-        self.capabilities['name'] = 'TOC'
-        hub_url = "%s:%s" % (self.username, self.key)
-        self.driver = webdriver.Remote(desired_capabilities=self.capabilities,
-                                       command_executor = ("http://%s@ondemand.saucelabs.com:80/wd/hub" % hub_url))
-        self.jobid = self.driver.session_id
-        print("Sauce Labs job: https://saucelabs.com/jobs/%s" % self.jobid)
-        self.driver.implicitly_wait(30)
+    def job_name(self):
+        return 'TOC test'
 
     def test_toc(self):
         self.driver.get('http://localhost:8000/1005')
-        drawer_toggle = self.driver.find_element_by_id('panel-link')
+        drawer_toggle = WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.find_element_by_id('panel-link'))
         drawer_toggle.click()
 
         # toggle arrow should switch
@@ -56,9 +39,6 @@ class ExampleTest(unittest.TestCase):
 
         # make sure that the current class has been removed from the prev section
         self.assertNotIn('current', toc_link_1005_1.get_attribute('class'))
-
-    def tearDown(self):
-        self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
