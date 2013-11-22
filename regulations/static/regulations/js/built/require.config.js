6a5c38725bc94cbcc7c248e599dd5fe5ef560a06
Adam Scott <adam.scott@cfpb.gov>
2013-11-22 13:10:20 -0200

    update modernizr to check for font-face support

--- regulations/static/regulations/js/built/require.config.js
+++ regulations/static/regulations/js/built/require.config.js
@@ -1,1 +1,59 @@
-var require={paths:{jquery:"./lib/jquery/jquery",underscore:"./lib/underscore/underscore",backbone:"./lib/backbone/backbone",samplejson:"../tests/grunt/js/fixtures/sample-json","jquery-scrollstop":"./lib/jquery.scrollstop/index",queryparams:"./lib/backbone.queryparams/index","definition-view":"./views/sidebar/definition-view","sub-head-view":"./views/header/sub-head-view","sidebar-module-view":"./views/sidebar/sidebar-module-view","toc-view":"./views/drawer/toc-view","sidebar-view":"./views/sidebar/sidebar-view","sidebar-head-view":"./views/sidebar/sidebar-head-view","reg-view":"./views/main/reg-view",konami:"./lib/konami/index","analytics-handler":"./views/analytics-handler-view","header-view":"./views/header/header-view","section-footer-view":"./views/main/section-footer-view","drawer-view":"./views/drawer/drawer-view","history-view":"./views/drawer/history-view","search-view":"./views/drawer/search-view","sxs-list-view":"./views/sidebar/sxs-list-view","sidebar-list-view":"./views/sidebar/sidebar-list-view","sxs-view":"./views/breakaway/sxs-view","search-results-view":"./views/main/search-results-view","main-view":"./views/main/main-view","permalink-view":"./views/sidebar/permalink-view","search-model":"./models/search-model","meta-model":"./models/meta-model","reg-model":"./models/reg-model","sxs-model":"./models/sxs-model","regs-router":"./router","regs-helpers":"./helpers","folder-model":"./models/folder-model"},shim:{underscore:{deps:["jquery"],exports:"_"},backbone:{deps:["underscore","jquery"],exports:"Backbone"},konami:{exports:"Konami"},"jquery-scrollstop":{deps:["jquery"]}}};
+var require = { "paths" :
+{
+        "jquery": "./lib/jquery/jquery",
+        "underscore": "./lib/underscore/underscore",
+        "backbone": "./lib/backbone/backbone",
+        "samplejson": "../tests/grunt/js/fixtures/sample-json",
+        "jquery-scrollstop": "./lib/jquery.scrollstop/index",
+        "queryparams": "./lib/backbone.queryparams/index",
+        "definition-view": "./views/sidebar/definition-view",
+        "sub-head-view": "./views/header/sub-head-view",
+        "sidebar-module-view": "./views/sidebar/sidebar-module-view",
+        "toc-view": "./views/drawer/toc-view",
+        "sidebar-view": "./views/sidebar/sidebar-view",
+        "sidebar-head-view": "./views/sidebar/sidebar-head-view",
+        "reg-view": "./views/main/reg-view",
+        "konami": "./lib/konami/index",
+        "analytics-handler": "./views/analytics-handler-view",
+        "header-view": "./views/header/header-view",
+        "section-footer-view": "./views/main/section-footer-view",
+        "drawer-view": "./views/drawer/drawer-view",
+        "history-view": "./views/drawer/history-view",
+        "search-view": "./views/drawer/search-view",
+        "sxs-list-view": "./views/sidebar/sxs-list-view",
+        "sidebar-list-view": "./views/sidebar/sidebar-list-view",
+        "sxs-view": "./views/breakaway/sxs-view",
+        "search-results-view": "./views/main/search-results-view",
+        "main-view": "./views/main/main-view",
+        "permalink-view": "./views/sidebar/permalink-view",
+        "search-model": "./models/search-model",
+        "meta-model": "./models/meta-model",
+        "reg-model": "./models/reg-model",
+        "sxs-model": "./models/sxs-model",
+        "regs-router": "./router",
+        "regs-helpers": "./helpers",
+        "folder-model": "./models/folder-model"
+    }
+, "shim":
+{
+        "underscore": {
+            "deps": [
+                "jquery"
+            ],
+            "exports": "_"
+        },
+        "backbone": {
+            "deps": [
+                "underscore",
+                "jquery"
+            ],
+            "exports": "Backbone"
+        },
+        "konami": {
+            "exports": "Konami"
+        },
+        "jquery-scrollstop": {
+            "deps": ["jquery"]
+        }
+}
+}
