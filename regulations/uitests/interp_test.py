cd91d70cb789abb30a56a6ff2253740399ef0d16
Theresa Summa <theresa.summa@cfpb.gov>
2013-11-25 19:33:34 -0200

    add interpretations test

---
+++ regulations/uitests/interp_test.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+from base_test import BaseTest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+class InterpTest(BaseTest, unittest.TestCase):
+
+    def job_name(self):
+        return 'Interp test'
+
+    def test_interps(self):
+        self.driver.get('http://localhost:8000/1005-2/2012-12121')
+        interp_dropdown = WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.find_element_by_xpath('//*[@id="1005-2-h"]/section'))
+
+        # interp should know who it belongs to
+        self.assertEquals(interp_dropdown.get_attribute('data-interp-for'), '1005-2-h')
+
+        # interp section should know what is in it
+        self.assertEquals(interp_dropdown.get_attribute('data-interp-id'), '1005-18-a')
+
+        # should have the appropriate header
+        self.assertTrue(u'OFFICIAL INTERPRETATION TO 18' in interp_dropdown.text)
+
+        # body should be hidden
+        interp_text = self.driver.find_element_by_xpath('//*[@id="1005-2-h"]/section/section')
+        interp_dropdown.click()
+
+        WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.find_element_by_css_selector('.inline-interpretation.open'))
+
+        # header should update
+        self.assertTrue(u'HIDE' in interp_dropdown.text)
+
+        # should contain the appropriate reg section
+        self.assertTrue(u'clicked. A finances centripetally curiousest stronghold cemeteries' in interp_text.text)
+
+        self.driver.find_element_by_xpath('//*[@id="1005-2-h"]/section/header/a').click()
+
+        WebDriverWait(self.driver, 10).until(
+            lambda driver: driver.find_element_by_css_selector('.inline-interpretation:not(.open)'))
+
+        # header should reflect close
+        self.assertTrue(u'SHOW' in interp_dropdown.text)
+
+if __name__ == '__main__':
+    unittest.main()
