f25fea1c7b7fad61a3665c385c389b6beb8e69ac
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2013-12-02 19:18:00 +0000

    add test

--- regulations/tests/views_partial_search_tests.py
+++ regulations/tests/views_partial_search_tests.py
@@ -41,6 +41,28 @@ class PartialSearchTest(TestCase):
         self.assertTrue('3333' in response.content)
         self.assertTrue('Consumer&#39;s' in response.content)
 
+    @patch('regulations.views.partial_search.api_reader')
+    @patch('regulations.views.partial_search.fetch_grouped_history')
+    def test_root_info(self, fetch_grouped_history, api_reader):
+        api_reader.ApiReader.return_value.search.return_value = {
+            'total_hits': 3,
+            'results': [
+                {'label': ['111', '22'], 'text': 'tttt', 'version': 'vvv', 'title':"consumer's"},
+                {'label': ['111', '24', 'a'], 'text': 'o', 'version': 'vvv'},
+                {'label': ['111'], 'text': 'more', 'version': 'vvv'}
+            ]
+        }
+        fetch_grouped_history.return_value = [
+            {'notices': [{'document_number': 'bbb',
+                          'effective_on': date(2012, 12, 12)}]},
+            {'notices': [{'document_number': 'ccc',
+                          'effective_on': date(2001, 1, 1)},
+                         {'document_number': 'vvv',
+                          'effective_on': date(2003, 4, 5)}],
+             'timeline': 'timeytimey'}
+        ]
+        response = Client().get('/partial/search/111?version=vvv&q=none')
+        self.assertTrue('2 results' in response.content)
 
     @patch('regulations.views.partial_search.api_reader')
     @patch('regulations.views.partial_search.fetch_grouped_history')
