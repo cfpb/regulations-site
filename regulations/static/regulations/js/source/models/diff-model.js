6292c1906601c7bca9b1242439b1d795a6687c8f
Theresa Summa <theresa.summa@cfpb.gov>
2013-12-07 03:12:58 -0200

    adding in diff files

---
+++ regulations/static/regulations/js/source/models/diff-model.js
@@ -0,0 +1,8 @@
+define('diff-model', ['underscore', 'backbone', 'meta-model'], function(_, Backbone, MetaModel) {
+    'use strict';
+    Backbone.DiffModel = MetaModel.extend({});
+
+    var diffModel = new Backbone.DiffModel({});
+
+    return diffModel;
+});
---
+++ regulations/static/regulations/js/source/views/main/diff-view.js
@@ -0,0 +1,9 @@
+define('diff-view', ['jquery', 'underscore', 'backbone', 'main-controller', './regs-router'], function($, _, Backbone, MainEvents, Router) {
+    'use strict';
+    var DiffView = Backbone.View.extend({
+
+    });
+
+    var diffView = new DiffView();
+    return diffView;
+});
