0bee1266314d9ea5f03399883f5650127c7a0159
Adam Scott <adam.scott@cfpb.gov>
2013-12-12 16:24:34 -0500

    remove test from watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,7 +165,7 @@ module.exports = function(grunt) {
     watch: {
       gruntfile: {
         files: ['Gruntfile.js', '<%= env.frontEndPath %>/css/less/*.less', '<%= env.frontEndPath %>/css/less/module/*.less', '<%= env.frontEndPath %>/css/less/media-queries/breakpoints/*.less','<%= env.frontEndPath %>/js/tests/specs/*.js', '<%= env.frontEndPath %>/js/source/*.js', '<%= env.frontEndPath %>/js/source/views/*.js', '<%= env.frontEndPath %>/js/source/views/*/*.js', '<%= env.frontEndPath %>/js/tests/functional/*.js'],
-        tasks: ['less', 'test']
+        tasks: ['less']
       }
     }
   });
