182fc6132cc067af13bc12ed5f219b38d312e00f
CM Lubinski <cm.lubinski@gmail.com>
2013-12-31 00:06:16 +0100

    Update formatting test

--- regulations/tests/layers_formatting_tests.py
+++ regulations/tests/layers_formatting_tests.py
@@ -2,30 +2,29 @@ from unittest import TestCase
 
 from mock import patch
 
-from regulations.generator.layers.formatting import TableLayer
+from regulations.generator.layers.formatting import FormattingLayer
 
 
-class TableLayerTest(TestCase):
+class FormattingLayerTest(TestCase):
     @patch('regulations.generator.layers.formatting.loader')
     def test_apply_layer(self, loader):
-        tl = TableLayer()
         render = loader.get_template.return_value.render
 
-        tl.apply_layer('Contains\nNo\nTables', '111-22')
+        table_data = {"table": "data"}
+        data = {'111-1': [],
+                '111-2': [{}], 
+                '111-3': [{'text': 'original', 'locations': [0, 2],
+                                   'table_data': table_data}]}
+        fl = FormattingLayer(data)
+        self.assertEqual([], fl.apply_layer('111-0'))
+        self.assertEqual([], fl.apply_layer('111-1'))
+        self.assertEqual([], fl.apply_layer('111-2'))
         self.assertFalse(render.called)
 
-        table = '|Header1|Header2|Header3|\n'
-        table += '|---|---|---|\n'
-        table += '|Cell1-1|Cell1-2|Cell1-3|\n'
-        table += '|Cell2-1|Cell2-2|Cell2-3|\n'
-        table += '|Cell3-1|Cell3-2|Cell3-3|'
-        tl.apply_layer(table, '111-22')
+        result = fl.apply_layer('111-3')
+        self.assertEqual(len(result), 1)
+        self.assertEqual('original', result[0][0])
+        self.assertEqual([0, 2], result[0][2])
         self.assertTrue(render.called)
-
         context = render.call_args[0][0]
-        self.assertEqual(['Header1', 'Header2', 'Header3'], context['header'])
-        self.assertEqual(3, len(context['rows']))
-        r1, r2, r3 = context['rows']
-        self.assertEqual(['Cell1-1', 'Cell1-2', 'Cell1-3'], r1)
-        self.assertEqual(['Cell2-1', 'Cell2-2', 'Cell2-3'], r2)
-        self.assertEqual(['Cell3-1', 'Cell3-2', 'Cell3-3'], r3)
+        self.assertEqual(context['table'], 'data')
