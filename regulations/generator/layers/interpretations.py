8796f978bc4c9b42b6ad271b16008903bb71216b
CM Lubinski <cm.lubinski@gmail.com>
2014-01-14 00:35:26 +0100

    Use subterp urls for the links in the definitions view and the slide-down interps

--- regulations/generator/layers/interpretations.py
+++ regulations/generator/layers/interpretations.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest
 
-#   Don't import PartialInterpView directly; this will cause an import cycle
+#   Don't import PartialInterpView or utils directly; causes an import cycle
 from regulations import views
 from regulations.generator.node_types import label_to_text, to_markup_id
 
@@ -12,6 +12,33 @@ class InterpretationsLayer(object):
     def __init__(self, layer, version=None):
         self.layer = layer
         self.version = version
+        self.toc_cache = {}
+
+    def determine_section_id(self, label):
+        """Subterps add an annoying wrinkle to interpretation links. We have
+        to figure out which subterp an interpretation is in, which is
+        exactly what this method is for"""
+        part = label[0]
+        if self.version and len(label) > 2:
+            key = (part, self.version)
+            if key not in self.toc_cache:
+                toc = views.utils.table_of_contents(part, self.version)
+                self.toc_cache[key] = toc
+            interpreted_section = label[:2]
+            has_subparts = False
+            for top in self.toc_cache[key]:
+                if top['index'] == interpreted_section:
+                    if top.get('is_section'):
+                        return '%s-Subpart-Interp' % part
+                    else:
+                        return '%s-Appendices-Interp' % part
+
+                for sub in top.get('sub_toc', []):
+                    #   In a subpart
+                    if sub['index'] == interpreted_section:
+                        return '-'.join(top['index'] + ['Interp'])
+        #   Default
+        return '%s-Interp' % part
 
     def apply_layer(self, text_index):
         """Return a pair of field-name + interpretation if one applies."""
@@ -37,9 +64,8 @@ class InterpretationsLayer(object):
                     'markup': response.content,
                 }
 
-                #  exclude 'Interp'
-                ref_parts = reference.split('-')[:-1]
-                interp['section_id'] = '%s-Interp' % ref_parts[0]
+                ref_parts = reference.split('-')
+                interp['section_id'] = self.determine_section_id(ref_parts)
 
                 context['interps'].append(interp)
 
