1f92d863f928395edb48eb54ae1bd1a7df9d8f8e
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-02-20 22:35:05 +0000

    PEP8

--- regulations/tests/views_partial_definitions_tests.py
+++ regulations/tests/views_partial_definitions_tests.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from django.core.urlresolvers import reverse, resolve
 from django.template import loader, Context
 
+
 class PartialDefinitionTests(TestCase):
     urls = 'regulation.urls'
 
@@ -9,11 +10,11 @@ class PartialDefinitionTests(TestCase):
         t = loader.get_template('regulations/partial-definition.html')
 
         node = {
-            'section_id':'102-4',
-            'label_id':'202-2-a',
-            'children': [{'label_id':'202-2-a-1'}]}
+            'section_id': '102-4',
+            'label_id': '202-2-a',
+            'children': [{'label_id': '202-2-a-1'}]}
 
-        context_dict = {'node':node, 'version':'2012-1223'}
+        context_dict = {'node': node, 'version': '2012-1223'}
         response = t.render(Context(context_dict))
         best_viewed = 'This definition is best viewed in its original location'
         self.assertTrue(best_viewed in response)
@@ -26,7 +27,7 @@ class PartialDefinitionTests(TestCase):
             'label_id': '202-2-a',
             'marked_up': 'This term is defined carefully'}
 
-        context_dict = {'node':node, 'version':'2012-1223'}
+        context_dict = {'node': node, 'version': '2012-1223'}
         response = t.render(Context(context_dict))
         carefully = 'This term is defined carefully'
         self.assertTrue(carefully in response)
