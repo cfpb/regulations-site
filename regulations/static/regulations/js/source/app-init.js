643dd65e89742c2ec3533e9a549854295958ef42
Theresa Summa <theresa.summa@cfpb.gov>
2014-03-26 19:39:58 -0300

    build

--- regulations/static/regulations/js/source/app-init.js
+++ regulations/static/regulations/js/source/app-init.js
@@ -1,7 +1,7 @@
 // Module called on app load, once doc.ready
 //
 /* jshint unused: false */
-define(['jquery', 'underscore', 'backbone', 'main-view', './regs-router', 'sidebar-view', 'header-view', 'drawer-view', 'konami'], function($, _, Backbone, MainView, Router, SidebarView, HeaderView, DrawerView, konami) {
+define(['jquery', 'underscore', 'backbone', 'main-view', './regs-router', 'sidebar-view', 'header-view', 'drawer-view', 'konami'], function($, _, Backbone, MainView, Router, SidebarView, HeaderView, DrawerView, Konami) {
     'use strict';
     return {
         // Purgatory for DOM event bindings that should happen in a View
@@ -14,14 +14,19 @@ define(['jquery', 'underscore', 'backbone', 'main-view', './regs-router', 'sideb
 
             // sssshhhhhhh
             new Konami(function() {
-                var $body = $('body').prepend('<div id="modal-overlay" class="loading"></div><div id="modal"><div id="modal-intro">Made for <span class="love">Tom Kearney</span> with <span class="love">love</span> by:</div><ul><li>Jen Ehlers</li><li>Shashank Khandelwal</li><li>CM Lubinski</li><li>Adam Scott</li><li>Theresa Summa</li><li>John Yuda</li><div id="x">x</div></div>');
+                var $body = $('body').prepend('<div id="modal-overlay" class="loading"></div><div id="modal"><div id="modal-intro">Made for <span class="love">Tom Kearney</span> with <span class="love">love</span> by:</div><ul><li>Jen Ehlers</li><li>Shashank Khandelwal</li><li>CM Lubinski</li><li>Adam Scott</li><li>Theresa Summa</li><li>John Yuda</li><div id="x">x</div></div>'),
+                    topval = document.documentElement.clientHeight / 2 - 200;
+
+                if (topval < 50) {
+                    topval = 50;
+                }
 
                 $('#modal-overlay')
                     .css('height', $body.height())
                     .css('width', document.documentElement.clientWidth);
 
                 $('#modal')
-                    .css('top', document.documentElement.clientHeight / 2 - 200 || 50)
+                    .css('top',  topval)
                     .css('left', document.documentElement.clientWidth / 2 - 200);
 
                 $('#x').on('click', function() {
