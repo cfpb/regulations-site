0ba8483a5dd96c6e91d82afafa8db24de5daf1d7
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-04-18 17:55:33 +0000

    Changed universal landing page to be dynamic, which also allows the generic page to be more flushed out

--- regulations/generator/versions.py
+++ regulations/generator/versions.py
@@ -1,9 +1,29 @@
 from datetime import datetime
+from collections import defaultdict
 
 from django.conf import settings
 
 from regulations.generator import api_reader
 from regulations.generator.layers.utils import convert_to_python
+
+
+def fetch_regulations_and_future_versions():
+    """ Returns a dict for all the regulations in the API. The dict includes
+    lists of future versions for each regulation. """
+    client = api_reader.ApiReader()
+    all_versions = client.all_regulations_versions()
+    all_versions = convert_to_python(all_versions)
+
+    today = datetime.today()
+    regulations_future = defaultdict(list)
+
+    #We're only interested in future endpoint versions
+    for v in all_versions['versions']:
+        if v['regulation'] not in regulations_future:
+            regulations_future[v['regulation']] = []
+        if 'by_date' in v:
+            regulations_future[v['regulation']].append(v)
+    return regulations_future
 
 
 def fetch_grouped_history(part):
--- regulations/views/universal_landing.py
+++ regulations/views/universal_landing.py
@@ -1,13 +1,49 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.template.loader import select_template
 from regulations.views import utils
 
+from regulations.generator import versions
+
+
+def filter_future_amendments(versions):
+    today = datetime.today()
+    amendments = [v for v in versions if v['by_date'] > today]
+    amendments.sort(key=lambda v: v['by_date'])
+    return amendments
+
 def universal(request):
-   context = {} 
-   utils.add_extras(context)
-   c = RequestContext(request, context)
-   t = select_template([
-        'regulations/universal_landing.html',
-        'regulations/generic_universal.html'])
-   return HttpResponse(t.render(c))
+    context = {} 
+    utils.add_extras(context)
+
+    all_versions = versions.fetch_regulations_and_future_versions()
+    reg_parts = sorted(all_versions.keys())
+    regs = []
+    today = datetime.today()
+
+    for part in reg_parts:
+        version = all_versions.get(part)[0]['version']
+        reg_meta = utils.regulation_meta(part, version, True)
+        first_section = utils.first_section(part, version)
+
+        amendments = filter_future_amendments(all_versions.get(part, None))
+        
+        reg = {'part': part, 
+               'meta': reg_meta,
+               'reg_first_section': first_section,
+               'amendments': amendments}
+
+        regs.append(reg)
+
+    context['regulations'] = regs
+    context['cfr_title_text'] = regs[0]['meta']['cfr_title_text']
+    context['cfr_title_number'] = utils.to_roman(
+        regs[0]['meta']['cfr_title_number'])
+
+    c = RequestContext(request, context)
+    t = select_template([
+         'regulations/universal_landing.html',
+         'regulations/generic_universal.html'])
+    return HttpResponse(t.render(c))
