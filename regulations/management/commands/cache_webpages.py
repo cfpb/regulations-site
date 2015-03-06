495492046b2be471a346abfc51014433938ef176
DarthDippy <lucaspfaff79@gmail.com>
2015-03-06 07:48:54 -0500

    code cleanup

--- regulations/management/commands/cache_webpages.py
+++ regulations/management/commands/cache_webpages.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from BeautifulSoup import BeautifulSoup
-import urllib2
+import requests
 from urlparse import urlparse
 import threading
 import sys
@@ -8,39 +8,39 @@ import time
 
 
 class Command(BaseCommand):
-    args = "<url where eregulations is located> <optional- comma seperated list of regulation locations to be parsed >  "
+    args = "<url where eregulations is located> <optional- comma seperated list of regulation locations to be parsed >"
     help = 'call every page in eregulations allowing the pages to be cached'
 
-    def access_url(obj,*args):
+    def access_url(self, *args):
 
         try:
-            without_anchor = args[0].split("#")[0]
-            temp = urllib2.urlopen(without_anchor)
-            temp.read()
-            temp.close()
-            print "Cached: " + without_anchor
-        except Exception, errtxt:
-            print "    Failed: " + without_anchor
-            print "    " + str(errtxt)
+            req = requests.get(args[0])
 
+            status_txt = "Cached"
+            if req.status_code != 200:
+                status_txt = "Failed"
+
+            msg = "{0} (status {1}): {2}".format(status_txt, str(req.status_code), req.url)
+            self.stdout.write(msg)
+        except Exception, errtxt:
+            self.stderr.write("    Failed: " + args[0])
+            self.stderr.write("    " + str(errtxt))
 
     def get_main_reg_list(self):
         try:
             regulations_links = []
-            html = urllib2.urlopen( self.full_url ).read()
+            html = requests.get(self.full_url).text
             soup = BeautifulSoup(html)
-            reg_list = soup.find( "ul", { "class" : "reg-list" } )
+            reg_list = soup.find("ul", {"class": "reg-list"})
 
             for link in reg_list.findAll("li"):
                 regulations_links.append(link.find("a")["href"])
 
-            print "Got List of Regulations "
             return regulations_links
 
         except Exception, errtxt:
-            print "main page failed to load"
-            print "    " + str(errtxt)
-
+            self.stderr.write("main page failed to load")
+            self.stderr.write("    " + str(errtxt))
 
     #figure out the partial path depending on root location of eregs
     def get_partial_url(self,  href):
@@ -48,14 +48,15 @@ class Command(BaseCommand):
         href = href.split("/")
 
         if self.full_url != self.base_url:
-            href[1] = href[1] + "/partial"
+            href[1] += "/partial"
         else:
-            href[0] = href[0] + "/partial"
+            href[0] += "/partial"
 
         href = '/'.join(href)
 
-        return self.base_url  + href
+        href = href.split("#")[0]
 
+        return self.base_url + href
 
     def handle(self, *args, **options):
 
@@ -63,14 +64,13 @@ class Command(BaseCommand):
         self.base_url = url.scheme + "://" + url.netloc
         self.full_url = sys.argv[2]
 
-        print self.base_url
-        print self.full_url
+        self.stdout.write("Base Url:" + self.base_url)
+        self.stdout.write("eRegs location:" + self.full_url)
+
         self.regulations = None
 
         if len(sys.argv) > 3:
             self.regulations = sys.argv[3]
-
-
 
         try:
             if not self.regulations:
@@ -79,25 +79,25 @@ class Command(BaseCommand):
                 regulations_links = self.regulations.split(",")
 
             for link in regulations_links:
-                print "Getting NAV links from " + self.base_url + link
+                self.stdout.write("Getting NAV links from " + self.base_url + link)
 
-                reg_nav = urllib2.urlopen(self.base_url+link)
+                reg_nav = requests.get(self.base_url+link).text
                 soup = BeautifulSoup(reg_nav)
                 reg_soup = soup.findAll("a")
 
                 for a in reg_soup:
                     if a.has_key('data-section-id'):
-
-                        partial = self.get_partial_url( a["href"])
+                        partial = self.get_partial_url(a["href"])
 
                         thread_count = len(threading.enumerate())
 
                         #process 5 web pages at time. Some servers hate a lot of requests at once.
+                        #slow it down so the threads can catch up
                         if thread_count <= 5:
-                            threading.Thread(target=self.access_url, args=(partial, ) ).start()
+                            threading.Thread(target=self.access_url, args=(partial, )).start()
                         else:
-                            print "Currently Processing " + str(thread_count) + " Urls"
-                            print "Waiting..."
+                            self.stdout.write("Currently Processing " + str(thread_count) + " Urls")
+                            self.stdout.write("Waiting...")
                             time.sleep(thread_count * 2)
 
                 #let the threads catch up before doing the next batch
@@ -105,7 +105,7 @@ class Command(BaseCommand):
                     time.sleep(len(threading.enumerate()) * 2)
 
         except Exception, errtxt:
-            print "    " + str(errtxt)
+            self.stderr.write("    " + str(errtxt))
 
 
 
