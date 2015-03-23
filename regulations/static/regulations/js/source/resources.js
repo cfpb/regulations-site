05d6bf4a07b0cd5ed1e29872816d23a764f1bed7
Kimberly Munoz <kimberly.munoz@cfpb.gov>
2015-03-23 18:04:40 -0400

    Fixes to Resources.js

--- regulations/static/regulations/js/source/resources.js
+++ regulations/static/regulations/js/source/resources.js
@@ -3,9 +3,9 @@ var $ = require('jquery');
 
 module.exports = {
   versionElements: {
-    toc: $('nav#toc'),
-    regLandingPage: $('section[data-base-version]'),
-    timelineList: $('#timeline li.current'),
-    diffToc: $('#table-of-contents')
+    toc: 'nav#toc',
+    regLandingPage: 'section[data-base-version]',
+    timelineList: '#timeline li.current',
+    diffToc: '#table-of-contents'
   }
 };
