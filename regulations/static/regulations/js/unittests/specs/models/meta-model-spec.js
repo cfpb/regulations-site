c48b4a9ba9ea3f567c6374e432028961b5ec0ad8
Adam Scott <adam.scott@cfpb.gov>
2015-03-26 10:10:32 -0400

    introduce tests for the set method

--- regulations/static/regulations/js/unittests/specs/models/meta-model-spec.js
+++ regulations/static/regulations/js/unittests/specs/models/meta-model-spec.js
@@ -21,14 +21,20 @@ describe('MetaModel', function() {
         });
     });
 
-    it('.has is called correctly.', function() {
+    it('has is called correctly.', function() {
         expect(this.metamodel.has).to.be.ok; // Returns ok
         expect(this.metamodel.has()).to.be.not.ok; // Returns ok
     });
 
-    it('.has can tell if an id exists', function() {
-        expect(this.metamodel.has('1005-2-a')).to.be.ok;
+    it('has can tell if an id exists', function() {
+        expect(this.metamodel.has('1005-2-a')).to.be.ok; // Returns true. '1005-2-a' is in the content.
         expect(this.metamodel.has('foo')).to.be.not.ok; // Returns false. Can't find foo in content.
+    });
+
+    it('set returns true for cached & formatted reg content', function() {
+        expect(this.metamodel.set('1005-2-a', '<li id="1005-2-a">Paragraph content</li>')).to.be.ok;
+        expect(this.metamodel.set('1005-4-a', '<li id="1005-4-a">Paragraph content</li>')).to.be.ok;
+        expect(this.metamodel.set('', '')).to.be.not.ok;
     });
 
 });
