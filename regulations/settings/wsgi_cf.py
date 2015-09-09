f4c5b018036fc1fea810166c972cc0073c5e26db
CM Lubinski <christopher.lubinski@gsa.gov>
2015-09-09 22:26:12 +0000

    Remove CF files

--- regulations/settings/wsgi_cf.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "regulations.settings.base")
-# important that the whitenoise import is after the line above
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(get_wsgi_application())
