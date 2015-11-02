77b3d90d0946a0e609f0af19566c0cda30a0e06a
Adam Scott <adam.scott@cfpb.gov>
2015-11-02 17:18:52 -0500

    set up section-footer-view test

---
+++ regulations/static/regulations/js/unittests/specs/views/main/section-footer-view-spec.js
@@ -0,0 +1,37 @@
+require('../../../setup');
+var sinon = require( 'sinon' );
+
+describe('Section Footer View:', function () {
+
+  var view, $, e, FooterView, Router;
+
+  before(function () {
+    $ = require('jquery');
+    Router= require('../../../../source/router');
+    FooterView = require('../../../../source/views/main/section-footer-view');
+    sandbox = sinon.sandbox.create();
+  });
+
+  beforeEach(function(){
+    //  Adding a simplified version of the thing we want to test.
+    $( 'body' ).html(
+
+    );
+
+    // create a new instance of the view
+    view = new FooterView();
+
+    Router.hasPushState = false;
+
+
+  });
+
+  it('should construct a view', function() {
+     console.log(Router.hasPushState);
+    expect(view).to.be.defined;
+    expect(view.events).to.be.defined;
+    expect(view.events).to.deep.equal({'click .navigation-link': 'sendNavEvent'});
+    expect(view.events['click .navigation-link']).to.equal('sendNavEvent');
+  });
+
+});
