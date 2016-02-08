8e3750e87f543958f63f008979d37c1532de5f4d
John Donmoyer <jdonmoyer@gmail.com>
2016-02-08 11:30:00 -0600

    turn off caching during dev

--- regulations/settings/dev.py
+++ regulations/settings/dev.py
@@ -8,6 +8,10 @@ STATICFILES_DIRS = (
 )
 
 
+CACHES['default']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
+CACHES['eregs_longterm_cache']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
+CACHES['api_cache']['TIMEOUT'] = 5  # roughly per request
+
 OFFLINE_OUTPUT_DIR = '/tmp/'
 
 INSTALLED_APPS += (
